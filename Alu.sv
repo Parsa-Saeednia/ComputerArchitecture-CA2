389c92dc49260236cdb021e209ce860b081c46e2
Parsa <parsasaiednia@gmail.com>
2025-05-08 20:28:39 +0330

    ALU done, Report updated.

--- Alu.sv
+++ Alu.sv
@@ -1,4 +1,24 @@
-module Alu();
+module Alu(srcA, srcB, opCode, aluResult, zero);
+    parameter N = 32;
+
+    input [N - 1:0] srcA, srcB;
+    input [2:0] opCode;
+    output reg [N - 1:0] aluResult;
+    output zero;
+
+    assign zero = ~|aluResult;
+
+    always @(*) begin
+        case (opCode)
+            3'b000: aluResult = srcA + srcB;
+            3'b001: aluResult = srcA - srcB;
+            3'b010: aluResult = srcA & srcB;
+            3'b011: aluResult = srcA | srcB;
+            3'b101: aluResult = ($signed(srcA) < $signed(srcB)) ? 1 : 0;
+
+            default: aluResult = 0;
+        endcase
+    end
     
     
 endmodule
