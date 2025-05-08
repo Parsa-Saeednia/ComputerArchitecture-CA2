2c2a9d69dee716016bf11a641fb906937154f426
Parsa <parsasaiednia@gmail.com>
2025-05-08 19:52:02 +0330

    Changed to systemVerilog And Completed Adder and Mux modules.

---
+++ Mux.sv
@@ -0,0 +1,12 @@
+module Mux(inp, sel, out);
+    parameter N = 4;
+    parameter M = 32;
+    localparam S = $clog2(N);
+
+    input [M - 1:0] inp [0:N - 1];
+    input [S - 1:0] sel;
+    output [M - 1:0] out;
+
+    assign out = inp[sel];
+    
+endmodule
--- Mux.v
+++
@@ -1,4 +0,0 @@
-module Mux #(parameter N) (input [N - 1:0] inp0, input [N - 1:0] inp1, input sel, output wire [N - 1:0] out);
-    assign out = sel ? inp1 : inp0;
-    
-endmodule
